29b4785c3577d9be9d7a1c8a81f744eef9f71a74
angkrz89 <angkrz89@users.noreply.github.com>
2021-04-11 07:51:40 +0000

    zmiana

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -17,6 +17,8 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
@@ -24,6 +26,8 @@ def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "mgs":"' +
             msg + '"}')
 
+def format_to_xml(msg, imie):
+    return ('<imie>' + imie + '</imie>''<msg>' + msg + '</msg>')
 
 def plain_text(msg, imie):
     return imie + ' ' + msg
